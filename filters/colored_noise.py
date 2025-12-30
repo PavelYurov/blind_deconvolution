e3d7256a21d9ef8b861af07f838e71f3766cbb35
Pavel Yurov <pavel.yurov04@bk.ru>
2025-12-30 22:17:43 +0300

    изменены коментрарии в filters

--- filters/colored_noise.py
+++ filters/colored_noise.py
@@ -1,16 +1,12 @@
 """
-Методы генерации цветных шумов
-
-Содержит:
-    - Методы для генерации цветного шума
+Функции генерации цветного шума.
 
 Автор: Юров П.И.
-Оригинал: https://github.com/felixpatzelt/colorednoise/blob/master/colorednoise.py
 """
 
 from typing import Union, Iterable, Optional, Tuple
 from numpy import sqrt, newaxis, integer
-from numpy.fft import irfft, rfftfreq, irfft2, fft2, ifft2, fftshift, fftfreq, ifftshift
+from numpy.fft import irfft, rfftfreq, irfft2
 from numpy.random import default_rng, Generator, RandomState
 from numpy import sum as npsum
 import numpy as np
@@ -25,19 +21,23 @@ def powerlaw_psd_gaussian(
     """
     Гауссов (1/f)**beta шум.
 
-    Аргументы:
+    Параметры
+    ---------
+    exponent : float
+        Экспонента шума S(f) = (1 / f)**beta.
+        Розовый шум: beta = 1.
+        Коричневый шум: beta = 2.
+    size : Union[int, Iterable[int]]
+        Размер выходной марицы.
+    fmin : float
+        Порог минимальной частоты от 0.0 до 0.5.
+    random_state : Optional[Union[int, Generator, RandomState]]
+        Генератор случайных чисел.
 
-    exponent (float): Экспонента шума
-        S(f) = (1 / f)**beta
-        розовый шум:    exponent beta = 1
-        коричневый шум: exponent beta = 2
-    shape (int or iterable): Размер выходной марицы
-    fmin (Optional[float]): Порог минимальной частоты от 0.0 до 0.5
-    random_state (Optional[Union[int, Generator, RandomState]]): \
-        Генератор случайных чисел
-
-    Возвращает:
-        матрицу шума
+    Возвращает
+    ----------
+    np.ndarray
+        Матрица шума.
     """
     
     if isinstance(size, (integer, int)):
@@ -56,7 +56,7 @@ def powerlaw_psd_gaussian(
         raise ValueError("fmin must be chosen between 0 and 0.5.")
     
     s_scale = f    
-    ix   = npsum(s_scale < fmin)
+    ix = npsum(s_scale < fmin)
     if ix and ix < len(s_scale):
         s_scale[:ix] = s_scale[ix]
     s_scale = s_scale**(-exponent/2.)
@@ -68,7 +68,7 @@ def powerlaw_psd_gaussian(
     size[-1] = len(f)
 
     dims_to_add = len(size) - 1
-    s_scale     = s_scale[(newaxis,) * dims_to_add + (Ellipsis,)]
+    s_scale = s_scale[(newaxis,) * dims_to_add + (Ellipsis,)]
     
     normal_dist = _get_normal_distribution(random_state)
 
@@ -82,7 +82,7 @@ def powerlaw_psd_gaussian(
     si[..., 0] = 0
     sr[..., 0] *= sqrt(2)
     
-    s  = sr + 1J * si
+    s = sr + 1J * si
 
     y = irfft(s, n=samples, axis=-1) / sigma
     
@@ -90,7 +90,19 @@ def powerlaw_psd_gaussian(
 
 
 def _get_normal_distribution(random_state: Optional[Union[int, Generator, RandomState]]):
-    """Возвращает генератор случайных чисел с нормальным распределением"""
+    """
+    Возвращает генератор случайных чисел с нормальным распределением.
+
+    Параметры
+    ---------
+    random_state : Optional[Union[int, Generator, RandomState]]
+        Источник случайности.
+        
+    Возвращает
+    ----------
+    Callable
+        Функция генерации нормального распределения.
+    """
     normal_dist = None
     if isinstance(random_state, (integer, int)) or random_state is None:
         random_state = default_rng(random_state)
@@ -105,40 +117,44 @@ def _get_normal_distribution(random_state: Optional[Union[int, Generator, Random
     return normal_dist
 
 
-def color_noise_2d(shape: Tuple[int, int], 
-                  alpha: float = 1.0):
+def pink_noise_2d(shape: Tuple[int, int], alpha: float = 1.0) -> np.ndarray:
     """
     Генерирует 2D 1/f^a шум.
     
-    Аргументы:
-        shape (Tuple[int, int]): Размер выходного массива шума
-        alpha (float): Экспонента 'a' 1/f^a шума
+    Параметры
+    ---------
+    shape : Tuple[int, int]
+        Размер выходного массива шума.
+    alpha : float
+        Экспонента 'a' 1/f^a шума.
     
-    Возвращает:
-        Матрица шума
+    Возвращает
+    ----------
+    np.ndarray
+        Матрица шума.
     """
     height, width = shape
     
     white_noise = np.random.randn(height, width)
 
-    white_fft = fft2(white_noise)
+    white_fft = np.fft.fft2(white_noise)
 
-    freqs_y = fftfreq(height)
-    freqs_x = fftfreq(width)
+    freqs_y = np.fft.fftfreq(height)
+    freqs_x = np.fft.fftfreq(width)
     freq_mesh = np.meshgrid(freqs_y, freqs_x, indexing='ij')
 
     radial_freq = np.sqrt(freq_mesh[0]**2 + freq_mesh[1]**2)
 
-    radial_freq_shifted = fftshift(radial_freq)
-    white_fft_shifted = fftshift(white_fft)
+    radial_freq_shifted = np.fft.fftshift(radial_freq)
+    white_fft_shifted = np.fft.fftshift(white_fft)
     
     with np.errstate(divide='ignore', invalid='ignore'):
-        scaling = np.where(radial_freq_shifted == 0, 1.0, radial_freq_shifted**(-alpha))
+        scaling = np.where(radial_freq_shifted == 0, 1.0, radial_freq_shifted**(-alpha/2))
         pink_fft_shifted = white_fft_shifted * scaling
     
-    pink_fft = ifftshift(pink_fft_shifted)
-    pink_noise = np.real(ifft2(pink_fft))
+    pink_fft = np.fft.ifftshift(pink_fft_shifted)
+    pink_noise = np.real(np.fft.ifft2(pink_fft))
     
-    pink_noise = pink_noise / np.std(pink_noise)*255.0
+    pink_noise = pink_noise / np.std(pink_noise) * 255.0
     
     return pink_noise
